cf77ebeb985308837b18624aeb7c541873e1c654
Adam Baker <albcodingbootcamp@gmail.com>
2019-12-10 14:50:13 -0500

    updated routes in server.js and made changes to htmlroutes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,7 +3,7 @@ var path = require("path");
 module.exports = function(app) {
  
   // If no matching route is found default to home
-  app.get("*", function(req, res) {
+  app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 
